c6f49bf69e60e0ecd1ef854085d021e2707ab7cf
foo@bar.com <lioncolab>
2019-04-18 03:10:07 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -128,10 +128,10 @@ class Classifier {
     for (let i = 0; i < inputText.length; ++i) {
       const word = inputText[i];
       inputBuffer.set(this.wordIndex[word], 0, i);
-      //console.log(word, this.wordIndex[word], inputBuffer);
+      console.log(word, this.wordIndex[word], inputBuffer);
     }
     const input = inputBuffer.toTensor();
-    //console.log(input);
+    console.log(input);
 
     status('Running inference');
     const beginMs = performance.now();
